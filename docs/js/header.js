2bc4ecc5ae744dc7864d8487bebbe6522a021b9e
Teppei Torii <teppeichan9@gmail.com>
2021-06-27 15:46:06 +0900

    不要jsファイル削除,ヘッダーの表示位置をjsで制御化

---
+++ docs/js/header.js
@@ -0,0 +1,19 @@
+const header = document.getElementById('header');
+
+if (window.matchMedia('(min-width:768px)').matches){
+  window.addEventListener('scroll', function(event){
+    if (window.pageYOffset < 1000 || window.pageYOffset == 0) {
+      header.style.visibility = 'hidden';
+    } else {
+      header.style.visibility = 'visible';
+    }
+  });
+} else {
+  window.addEventListener('scroll', function(event){
+    if (window.pageYOffset < 330 || window.pageYOffset == 0) {
+      header.style.visibility = 'hidden';
+    } else {
+      header.style.visibility = 'visible';
+    }
+  });
+}
